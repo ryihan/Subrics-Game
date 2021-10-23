3a4534a4f0657ab6554d6049e2afac3e793a8d42
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-23 22:52:07 +0600

    Create StringAssets.cs

---
+++ Library/PackageCache/com.unity.collab-proxy@1.11.2/Editor/Collaborate/Assets/StringAssets.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Unity.Cloud.Collaborate.Assets
+{
+    /// <summary>
+    /// Static string resources used throughout the UI.
+    /// </summary>
+    internal static class StringAssets
+    {
+        public const string searchResults = "Search Results";
+        public const string cannotPublishWhileSearching = "Please clear the search before publishing changes.";
