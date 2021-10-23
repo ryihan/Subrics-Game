e44806aef7d865f320a70860bfaa7a155fc7522b
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-23 22:52:13 +0600

    Update StringAssets.cs

--- Library/PackageCache/com.unity.collab-proxy@1.11.2/Editor/Collaborate/Assets/StringAssets.cs
+++ Library/PackageCache/com.unity.collab-proxy@1.11.2/Editor/Collaborate/Assets/StringAssets.cs
@@ -9,3 +9,12 @@ namespace Unity.Cloud.Collaborate.Assets
     {
         public const string searchResults = "Search Results";
         public const string cannotPublishWhileSearching = "Please clear the search before publishing changes.";
+        public const string cannotPublishWhileConflicted = "Please fix the above conflicts before publishing.";
+        public const string cannotPublishWithoutFiles = "No files to publish.";
+        public const string cannotPublishWithIncomingChanges = "Please sync latest changes before publishing.";
+        public const string changeListFullHeader = "Publish Changes";
+        public const string changeListConflictedHeader = "Conflicted Items";
+        public const string changeListConflictedList = "conflicted";
+        public const string noticeNoRevisionsToDisplay = "No revisions to display.";
+        public const string noticeNoChangesToDisplay = "No changes to display.";
+        public const string noticeNoResultsForQuery = "No results found for this query.";
