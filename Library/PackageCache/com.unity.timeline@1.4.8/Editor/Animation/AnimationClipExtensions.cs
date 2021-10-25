2c060a977c8b34b015a91daac0921138bf8d9940
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 17:50:26 +0600

    Update AnimationClipExtensions.cs

--- Library/PackageCache/com.unity.timeline@1.4.8/Editor/Animation/AnimationClipExtensions.cs
+++ Library/PackageCache/com.unity.timeline@1.4.8/Editor/Animation/AnimationClipExtensions.cs
@@ -6,3 +6,15 @@ namespace UnityEditor.Timeline
     static class AnimationClipExtensions
     {
         public static UInt64 ClipVersion(this AnimationClip clip)
+        {
+            if (clip == null)
+                return 0;
+
+            var info = AnimationClipCurveCache.Instance.GetCurveInfo(clip);
+            var version = (UInt32) info.version;
+            var count = (UInt32) info.curves.Length;
+            var result = (UInt64) version;
+            result |= ((UInt64) count) << 32;
+            return result;
+        }
+
