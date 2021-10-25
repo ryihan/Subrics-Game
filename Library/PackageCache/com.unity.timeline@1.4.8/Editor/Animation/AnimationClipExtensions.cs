d7e23abf037cee382e7a027a673d87a143da0ddc
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 17:50:13 +0600

    Create AnimationClipExtensions.cs

---
+++ Library/PackageCache/com.unity.timeline@1.4.8/Editor/Animation/AnimationClipExtensions.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+namespace UnityEditor.Timeline
+{
+    static class AnimationClipExtensions
+    {
+        public static UInt64 ClipVersion(this AnimationClip clip)
