5bae996360278cdff5b1d81171b854d6d79bf970
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:49:58 +0600

    Create networkdevice.ts

---
+++ tslib/webgl/networkdevice.ts
@@ -0,0 +1,59 @@
+// Copyright (c) 2011-2012 Turbulenz Limited
+
+"use strict";
+
+//
+// WebGLNetworkDevice
+//
+class WebGLNetworkDevice implements NetworkDevice
+{
+    static version = 1;
+
+    WebSocketConstructor: any;  // prototype
+
+    createWebSocket(url:string, protocol?: string): WebSocket
+    {
+        var WebSocketConstructor = this.WebSocketConstructor;
+        if (WebSocketConstructor)
+        {
+            var ws : WebSocket;
+            if (protocol)
+            {
+                ws = new WebSocketConstructor(url, protocol);
+            }
+            else
+            {
+                ws = new WebSocketConstructor(url);
+            }
+            if (typeof ws.destroy === "undefined")
+            {
+                ws.destroy = function websocketDestroyFn()
+                {
+                    this.onopen = null;
+                    this.onerror = null;
+                    this.onclose = null;
+                    this.onmessage = null;
+                    this.close();
+                };
+            }
+            return ws;
+        }
+        else
+        {
+            return <WebSocket>null;
+        }
+    }
+
+    update()
+    {
+    }
+
+    static create(params: any): WebGLNetworkDevice
+    {
+        var nd = new WebGLNetworkDevice();
+        return nd;
+    }
+}
+
+WebGLNetworkDevice.prototype.WebSocketConstructor =
+    (window.WebSocket ? window.WebSocket : window.MozWebSocket);
