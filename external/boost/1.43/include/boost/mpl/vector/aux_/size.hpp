0b53a7cf30e74ba223ec9560de548f89346dfb53
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:38:48 +0600

    Update size.hpp

--- external/boost/1.43/include/boost/mpl/vector/aux_/size.hpp
+++ external/boost/1.43/include/boost/mpl/vector/aux_/size.hpp
@@ -2,3 +2,37 @@
 #ifndef BOOST_MPL_VECTOR_AUX_SIZE_HPP_INCLUDED
 #define BOOST_MPL_VECTOR_AUX_SIZE_HPP_INCLUDED
 
+
+#include <boost/mpl/size_fwd.hpp>
+#include <boost/mpl/vector/aux_/O1_size.hpp>
+#include <boost/mpl/vector/aux_/tag.hpp>
+#include <boost/mpl/aux_/config/typeof.hpp>
+#include <boost/mpl/aux_/config/ctps.hpp>
+
+namespace boost { namespace mpl {
+
+#if defined(BOOST_MPL_CFG_TYPEOF_BASED_SEQUENCES)
+
+template<>
+struct size_impl< aux::vector_tag >
+    : O1_size_impl< aux::vector_tag >
+{
+};
+
+#else
+
+#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+
+template< long N >
+struct size_impl< aux::vector_tag<N> >
+    : O1_size_impl< aux::vector_tag<N> >
+{
+};
+
+#endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+
+#endif // BOOST_MPL_CFG_TYPEOF_BASED_SEQUENCES
+
+}}
+
+#endif // BOOST_MPL_VECTOR_AUX_SIZE_HPP_INCLUDED
