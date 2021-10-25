3b4733c97b34642ee95e11a3aa4e5fa31272fcf7
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:02:30 +0600

    Create __init__.py

---
+++ scripts/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2009-2014 Turbulenz Limited
+"""
+Turbulenz framework for creating browser based games.
+"""
+
+__author__ = "Turbulenz Limited"
+__copyright__ = "Copyright (c) 2009-2014 Turbulenz Limited"
+
+TURBULENZ_ENGINE_VERSION = "1.2"
