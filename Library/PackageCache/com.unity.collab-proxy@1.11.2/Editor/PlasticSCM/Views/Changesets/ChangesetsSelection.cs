8097a282000c92091ca979de771560207c2e4349
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:00:07 +0600

    Create ChangesetsSelection.cs

---
+++ Library/PackageCache/com.unity.collab-proxy@1.11.2/Editor/PlasticSCM/Views/Changesets/ChangesetsSelection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using Codice.CM.Common;
+using Unity.PlasticSCM.Editor.UI.Tree;
+
+namespace Unity.PlasticSCM.Editor.Views.Changesets
+{
+    internal static class ChangesetsSelection
+    {
+        internal static void SelectChangesets(
+            ChangesetsListView listView,
+            List<RepObjectInfo> csetsToSelect,
