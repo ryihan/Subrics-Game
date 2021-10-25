d9489c05d199700560e11ad51ae5775b59cabb85
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:00:28 +0600

    Create ChangesetsTab.cs

---
+++ Library/PackageCache/com.unity.collab-proxy@1.11.2/Editor/PlasticSCM/Views/Changesets/ChangesetsTab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+using Codice.Client.Commands;
+using Codice.Client.Common.Threading;
+using Codice.CM.Common;
+using PlasticGui;
+using PlasticGui.WorkspaceWindow.QueryViews;
+using PlasticGui.WorkspaceWindow.QueryViews.Changesets;
+using Unity.PlasticSCM.Editor.UI;
+using Unity.PlasticSCM.Editor.UI.Progress;
+using Unity.PlasticSCM.Editor.UI.Tree;
+using Unity.PlasticSCM.Editor.Views.Diff;
+using Codice.Client.BaseCommands.EventTracking;
+
+namespace Unity.PlasticSCM.Editor.Views.Changesets
+{
+    internal class ChangesetsTab :
+        IRefreshableView,
+        IChangesetMenuOperations,
+        ChangesetsViewMenu.IMenuOperations
+    {
+        internal ChangesetsTab(
+            WorkspaceInfo wkInfo,
