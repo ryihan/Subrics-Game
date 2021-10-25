5d87bce7956bb0ceef85a3bf454a8a7c24ec1eb3
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:49:06 +0600

    Create touch.ts

---
+++ tslib/webgl/touch.ts
@@ -0,0 +1,29 @@
+// Copyright (c) 2012 Turbulenz Limited
+
+class Touch
+{
+    force: number;
+    identifier: number;
+    isGameTouch: boolean;
+    positionX: number;
+    positionY: number;
+    radiusX: number;
+    radiusY: number;
+    rotationAngle: number;
+
+    static create(params: any): Touch
+    {
+        var touch = new Touch();
+
+        touch.force         = params.force;
+        touch.identifier    = params.identifier;
+        touch.isGameTouch   = params.isGameTouch;
+        touch.positionX     = params.positionX;
+        touch.positionY     = params.positionY;
+        touch.radiusX       = params.radiusX;
+        touch.radiusY       = params.radiusY;
+        touch.rotationAngle = params.rotationAngle;
+
+        return touch;
+    }
+}
