ecc81d192ebaf834f48c6fc98ae5409887c021a2
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 18:35:40 +0600

    Create tag.hpp

---
+++ external/boost/1.43/include/boost/mpl/vector/aux_/tag.hpp
@@ -0,0 +1,17 @@
+
+#include <boost/mpl/aux_/config/typeof.hpp>
+#include <boost/mpl/aux_/nttp_decl.hpp>
+
+namespace boost { namespace mpl { namespace aux {
+
+struct v_iter_tag;
+
+#if defined(BOOST_MPL_CFG_TYPEOF_BASED_SEQUENCES)
+struct vector_tag;
+#else
+template< BOOST_MPL_AUX_NTTP_DECL(long, N) > struct vector_tag;
+#endif
+
+}}}
+
+#endif // BOOST_MPL_VECTOR_AUX_TAG_HPP_INCLUDED
