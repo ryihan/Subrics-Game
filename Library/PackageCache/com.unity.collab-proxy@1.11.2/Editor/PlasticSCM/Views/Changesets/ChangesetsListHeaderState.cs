4f793b035b83830790e27a7a41cb3dc3180414ed
raihan <54474184+ryihan@users.noreply.github.com>
2021-10-25 17:59:49 +0600

    Create ChangesetsListHeaderState.cs

---
+++ Library/PackageCache/com.unity.collab-proxy@1.11.2/Editor/PlasticSCM/Views/Changesets/ChangesetsListHeaderState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+using PlasticGui;
+using Unity.PlasticSCM.Editor.UI;
+using Unity.PlasticSCM.Editor.UI.Tree;
+
+namespace Unity.PlasticSCM.Editor.Views.Changesets
+{
+    internal enum ChangesetsListColumn
+    {
+        Name,
+        CreationDate,
+        CreatedBy,
+        Comment,
+        Branch,
+        Repository,
